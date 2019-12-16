1801d5ffdd9f517efe9e1ab601e33dbb5a726f5d
sandra <naimsandra9@gmail.com>
2019-12-16 21:58:32 +0200

    Js Basic 1 - Step 4

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,7 @@
+function myFunct() {
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+
+  alert("Hello " + name + " " + surname + " from " + city);
+}
