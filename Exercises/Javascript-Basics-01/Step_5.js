cf57aba66b53d9f27918bb5b1151cb2f616b81b0
sandra <naimsandra9@gmail.com>
2019-12-17 09:07:15 +0200

    Js Basic 1 - Step 5

---
+++ Exercises/Javascript-Basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function mult() {
+  var first_number = document.getElementById("first_number").value;
+  var second_number = document.getElementById("second_number").value;
+  var multi = first_number * second_number;
+  alert(multi);
+}
