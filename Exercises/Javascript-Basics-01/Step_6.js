7cf83030c0f559fffc74e7ffc6a79dfd549a3e52
sandra <naimsandra9@gmail.com>
2019-12-17 09:18:28 +0200

    Js Basic 1 - Step 6

---
+++ Exercises/Javascript-Basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function divide() {
+  var first_number = document.getElementById("first_number").value;
+  var second_number = document.getElementById("second_number").value;
+  //var divide = first_number / second_number;
+  alert(first_number % second_number);
+}
