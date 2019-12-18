bc334f4f74fc861d6bbab18a51b8fd5dc45707b6
sandra <naimsandra9@gmail.com>
2019-12-18 11:29:52 +0200

    Js Basic 3 - Step 3

---
+++ Exercises/Javascript-Basics-03/step 3/Step_3.js
@@ -0,0 +1,11 @@
+function fr() {
+  document.getElementById("text").style.color = "red";
+}
+
+function fg() {
+  document.getElementById("text").style.color = "green";
+}
+
+function fb() {
+  document.getElementById("text").style.color = "blue";
+}
