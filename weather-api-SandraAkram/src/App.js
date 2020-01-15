4096122d9d859673658ff8785784768cfaa5adf1
sandra <naimsandra9@gmail.com>
2020-01-15 17:22:05 +0200

    SandraAkram Weather-API

--- weather-api-SandraAkram/src/App.js
+++ weather-api-SandraAkram/src/App.js
@@ -14,6 +14,7 @@ import DetailedWeather from "./DetailedWeather/DetailedWeather";
 
 import React, { Component } from "react";
 import "./App.css";
+
 function getImg(id) {
   let val = "";
   if (id < 300) val = storm;
