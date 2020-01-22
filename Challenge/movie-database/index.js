6362ea3529b23e7ab792860bd4f73be3de0c49ad
sandra <naimsandra9@gmail.com>
2020-01-22 12:33:04 +0200

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -3,4 +3,14 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("ok");
 });
+app.get("/test", (req, res) => {
+  res.send({ status: 200, message: "ok" });
+});
+app.get("/time", (req, res) => {
+  var d = new Date();
+  var t1 = d.getHours();
+  var t2 = d.getMinutes();
+  res.send({ status: 200, message: `${t1}:${t2}` });
+});
+
 app.listen(3003, () => console.log("Example app listening on port 3003!"));
