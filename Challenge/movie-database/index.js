fe860ccde230d835f4417a599c7cebf537c62e13
sandra <naimsandra9@gmail.com>
2020-01-22 13:52:54 +0200

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -12,5 +12,21 @@ app.get("/time", (req, res) => {
   var t2 = d.getMinutes();
   res.send({ status: 200, message: `${t1}:${t2}` });
 });
+app.get(`/hello/:ID?`, (req, res) => {
+  var userID = req.params.ID;
+  res.send({ status: 200, message: "Hello ", userID });
+});
+app.get(`/search?:s?`, (req, res) => {
+  var userSearch = req.query.s;
+  if (userSearch) {
+    res.send({ status: 200, message: "ok", data: userSearch });
+  } else {
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+  }
+});
 
 app.listen(3003, () => console.log("Example app listening on port 3003!"));
