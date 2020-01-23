fcbae29c084ec20e365183ca3a9660b03384ab1d
sandra <naimsandra9@gmail.com>
2020-01-23 10:14:20 +0200

    step 6

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -42,11 +42,33 @@ app.get("/movies/update", (req, res) => {
   res.send({ status: 200, message: "update" });
 });
 
+app.get("/movies/read/by-date", (req, res) => {
+  res.send({ status: 200, message: sortedMov });
+});
+app.get("/movies/read/by-rating", (req, res) => {
+  res.send({ status: 200, message: sortedRat });
+});
+app.get("/movies/read/by-title", (req, res) => {
+  res.send({ status: 200, message: sortedTit });
+});
+
 const movies = [
   { title: "Jaws", year: 1975, rating: 8 },
   { title: "Avatar", year: 2009, rating: 7.8 },
   { title: "Brazil", year: 1985, rating: 8 },
   { title: "الإرهاب والكباب‎", year: 1992, rating: 6.2 }
 ];
+
+const sortedMov = movies.slice().sort((a, b) => a.year - b.year);
+const sortedRat = movies.slice().sort((a, b) => b.rating - a.rating);
+const sortedTit = movies.slice().sort(function(a, b) {
+  if (a.title < b.title) {
+    return -1;
+  }
+  if (a.title > b.title) {
+    return 1;
+  }
+  return 0;
+});
 
 app.listen(3003, () => console.log("Example app listening on port 3003!"));
